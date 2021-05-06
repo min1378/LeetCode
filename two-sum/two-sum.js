400663fcc307da8439e51ed170865e5145d67f42
min1378 <52685185+min1378@users.noreply.github.com>
2021-05-06 14:53:41 +0900

    Time: 76 ms (80.10%), Space: 38 MB (99.17%) - LeetHub

--- two-sum/two-sum.js
+++ two-sum/two-sum.js
@@ -3,13 +3,14 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    for(let i = 0; i<nums.length - 1; i++){
-        for(let j = i + 1; j<nums.length; j++){
-            if(nums[i] + nums[j] === target){
-                return [i, j]
-            }
-        }
-
+var twoSum = function (nums, target) {
+  const numberMap = {};
+  for (let i = 0; i < nums.length; i++) {
+    const number = nums[i];
+    const findNumber = target - number;
+    if (numberMap[findNumber] > -1) {
+      return [numberMap[findNumber], i];
     }
+    numberMap[number] = i;
+  }
 };
